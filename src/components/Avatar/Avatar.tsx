5cf8c68c3bb29caef89b5cae2785938c7f5f9161
AM-77 <amine.griche77@protonmail.ch>
2020-10-29 23:31:01 +0100

    Update Avatar Component

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -3,25 +3,21 @@ import PropTypes from 'prop-types';
 import './style.scss';
 
 type props = {
-  avatarUrl?: string;
+  avatarUrl: string;
   isLoading: boolean;
 };
 
 const Avatar: React.FC<props> = ({ avatarUrl, isLoading }) => {
   return (
-    <div className={`avatar ${isLoading ? 'shimmer' : ''}`}>
+    <div className={`avatar ${isLoading && 'shimmer'}`}>
       {avatarUrl && <img src={avatarUrl} alt="user avatar" />}
     </div>
   );
 };
 
 Avatar.propTypes = {
-  avatarUrl: PropTypes.string,
+  avatarUrl: PropTypes.string.isRequired,
   isLoading: PropTypes.bool.isRequired,
-};
-
-Avatar.defaultProps = {
-  avatarUrl: '',
 };
 
 export default Avatar;
