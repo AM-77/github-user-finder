1505dac19e9ea61f9a7e262214188c7673021700
AM-77 <amine.griche77@protonmail.ch>
2020-10-29 23:32:07 +0100

    add <Orgs/>

---
+++ src/components/Orgs/Orgs.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { IOrg } from '../../types';
+import './style.scss';
+
+type props = {
+  orgs?: IOrg[];
+  isLoading: boolean;
+};
+
+const Orgs: React.FC<props> = ({ orgs, isLoading }) => {
+  const renderOrgs = (organizations: IOrg[]) => {
+    if (organizations.length === 0)
+      return <p>You don’t belong to any organization yet!</p>;
+    return organizations.map(({ url, name, avatarUrl }) => (
+      <div className="org" key={url}>
+        <a href={url} title={name}>
+          <img src={avatarUrl} alt={name} />
+        </a>
+      </div>
+    ));
+  };
+
+  return (
+    <div className="organizations">
+      <h2>
+        Organizations <span>{orgs && orgs.length}</span>
+      </h2>
+      <div className={`orgs ${isLoading && 'shimmer'}`}>
+        {orgs && renderOrgs(orgs)}
+      </div>
+    </div>
+  );
+};
+
+Orgs.propTypes = {
+  orgs: PropTypes.arrayOf(
+    PropTypes.shape({
+      url: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      avatarUrl: PropTypes.string.isRequired,
+    }).isRequired
+  ),
+  isLoading: PropTypes.bool.isRequired,
+};
+
+Orgs.defaultProps = {
+  orgs: [],
+};
+
+export default Orgs;
